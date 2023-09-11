def813cac16483cd39aba7a142992e24c54301d1
spotmapp <130816297+spotmapp@users.noreply.github.com>
2023-09-10 21:27:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+import '.style.css'
 
+const $ = selector => document.querySelector(selector)
+
+const $js = $('#js')
+const $css = $('#css')
+const $html = $('#html')
+
+$js.addEventListener('input', update)
+$css.addEventListener('input', update)
+$html.addEventListener('input', update)
+
+function update (){
+  const html = createHtml()
+  $('iframe').setAttribute('srcdoc', html)
+}
+
+const createHtml = () =>{
+  const html =$html.value
+  const css =$css.value
+  const js =$js.value
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        ${css}
+      </head>
+      <body>
+        <script>
+          ${js}
+        </script>
+        ${html}
+      </body>
+    </html>
+  `
+}
